1b4d8ac8bfbecd9d7303486b24ee50805009d3f5
LightRangers <461565535@qq.com>
2019-05-15 23:59:45 +0800

    初始化的项目。三个基本页面

---
+++ models/db.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+HOST = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'todo'
+USERNAME = 'root'
+PASSWORD = 'qwe123'
+Db_Uri = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(
+    USERNAME, PASSWORD, HOST, PORT, DATABASE)
+
+engine = create_engine(Db_Uri)
+Base = declarative_base(engine)
+DBSession = sessionmaker(bind=engine)
+
+if __name__ == '__main__':
+    connection = engine.connect()
+    result = connection.execute('select 1')
+    print(result.fetchone())
