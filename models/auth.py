75efb46b7cc1c2e3a46d38acfaf6f92d1b76009b
LightRangers <461565535@qq.com>
2019-05-16 23:59:45 +0800

    增加post表

--- models/auth.py
+++ models/auth.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from sqlalchemy import (Column, Integer, String, DateTime)
+from sqlalchemy import (Column, Integer, String, DateTime, ForeignKey, )
 from models.db import Base
+from sqlalchemy.orm import relationship
 
 
 class User(Base):
@@ -10,6 +11,15 @@ class User(Base):
     password = Column(String(50))
     createtime = Column(DateTime, default=datetime.now)
     email = Column(String(80))
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    image_url = Column(String(200))
+    # 建立外键关系
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', backref='posts', uselist=False, cascade='all')
 
 
 if __name__ == '__main__':
